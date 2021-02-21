0ba228deb78ab3c0ea8e27478eb9baa0aba57e56
Jared Ackley <jrod3323@gmail.com>
2021-02-21 13:58:00 -0600

    add workout schema info and index.js

---
+++ models/Workout.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+    exercise: [
+        {
+            name: {
+                type: String,
+                trim: true,
+                required: "Exercise Name Is Required"
+            },
+
+            type: {
+                type: String,
+                trim : true,
+                required: "Exercise Type Is Required"
+            },
+
+            distance: {
+                type: Number, 
+            },
+
+            duration: {
+                type: Number,
+                required: "Workout Duration Is Required"
+            },
+
+            weight: {
+                type: Number,
+            },
+
+            sets: {
+                type: Number,
+            },
+
+            reps: {
+                type: Number,
+            }
+        }
+    ]
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
