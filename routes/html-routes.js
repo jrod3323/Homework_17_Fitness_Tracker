a32b363e193511e025a0c0f66067d9fd78218016
Jared Ackley <jrod3323@gmail.com>
2021-02-22 14:59:11 -0600

    change Workout.js file name

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ let path = require("path");
 
 module.exports = function (app) {
 
-  app.get("", function (req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
